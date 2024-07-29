e08bac224a437b9e0703a853ff46ecbe62b2e4f2
Christian Heimes <christian@python.org>
2024-07-29 08:13:29 +0200

    Major rework (#2)
    - drop 3.9 support
    - make `_ELFDeps` class internal
    - support streaming IO, zipfile, and tarfile
    - move settings to `ELFAnalyzeSettings`
    
    Signed-off-by: Christian Heimes <christian@python.org>

--- src/elfdeps/__init__.py
+++ src/elfdeps/__init__.py
@@ -1,5 +1,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
-__all__ = ("ELFDeps",)
+__all__ = (
+    "ELFAnalyzeSettings",
+    "ELFError",
+    "ELFInfo",
+    "SOInfo",
+    "analyze_elffile",
+    "analyze_file",
+    "analyze_tarmember",
+    "analyze_zipmember",
+)
 
-from ._elfdeps import ELFDeps
+from elftools.common.exceptions import ELFError
+
+from ._archives import (
+    analyze_tarmember,
+    analyze_zipmember,
+)
+from ._elfdeps import (
+    ELFAnalyzeSettings,
+    ELFInfo,
+    SOInfo,
+    analyze_elffile,
+    analyze_file,
+)
